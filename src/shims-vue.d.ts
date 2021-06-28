45721aaa2f2a9df44364e2d588fc5fbc4998dd4f
sabinawana <was@sml.at>
2021-06-28 09:36:03 +0200

    update for svg

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+declare module '*.svg' {
+  import type { DefineComponent } from 'vue';
+  const component: DefineComponent;
+  export default component;
+} 
